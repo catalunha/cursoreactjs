8aa9339cf0148ddb8a8cfc4ff6ab22b9b189acb8
catalunha <catalunha.mj@gmail.com>
2020-04-28 19:24:40 -0300

    aula09

--- script01.js
+++ script01.js
@@ -1,8 +1,21 @@
-var nome="MJ Catalunha";
-// comentario de linha
-/*
-comentario de
-varias 
-linhas
+function entrar() {
+    var area = document.getElementById('area');
+    var texto = prompt('Digite seu nome ?');
+    if (texto === '') {
+        alert('Digite seu nome!!');
+        area.innerHTML = 'Então bem vindo...';
+    } else {
+        area.innerHTML = texto;
+    }
+}
 
-*/
+function entrar2(nome) {
+    var area = document.getElementById('area2');
+    var sobrenome = prompt('Digite seu sobrenome ?');
+    if (sobrenome === '') {
+        alert('Digite seu nome!!');
+        area.innerHTML = 'Então bem vindo ' + nome;
+    } else {
+        area.innerHTML = 'Então bem vindo ' + nome + sobrenome;
+    }
+}
