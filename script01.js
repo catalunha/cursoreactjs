9e3a8a1d430d3679e56c830c458b48ec876649eb
catalunha <catalunha.mj@gmail.com>
2020-04-28 20:33:00 -0300

    aula14

--- script01.js
+++ script01.js
@@ -1,31 +1,6 @@
-var x = 0;
-while (x < 10) {
-    document.write('</br> O valor de X é ' + x);
-    x++;
+function acao() {
+    document.write('Executando...<br/>');
 }
+var timer = setInterval(acao,2000);
 
-for (var i = 0; i < 10; i++) {
-    document.write('</br> O valor de i é ' + i);
-}
-document.write('</br> Pedidos');
-document.write('</br> 0=suco');
-document.write('</br> 1=agua');
-document.write('</br> 2=pao');
-
-function pedir() {
-    y = prompt('Pedir ?');
-    switch (y) {
-        case '0':
-            document.write('</br> Pedido: suco ' + y);
-            break;
-        case '1':
-            document.write('</br> Pedido: agua ' + y);
-            break;
-        case '2':
-            document.write('</br> Pedido: pao ' + y);
-            break;
-        default:
-            break;
-    }
-}
-console.log('ola');
+// setTimeout(acao,2000);
