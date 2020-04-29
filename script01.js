066043aee486125def551ff9dbf49a84ca572c55
catalunha <catalunha.mj@gmail.com>
2020-04-29 08:00:12 -0300

    iniciando proj react

--- script01.js
+++
@@ -1,7 +0,0 @@
-// let nomes = ['aa','bb','cc'];
-// console.log(nomes.some(nome=> nome==='aa'));
-
-let nomes = [{ nome: 'aa', pos: 10 }, { nome: 'bb', pos: 20 }];
-console.log(nomes);
-console.log(nomes.every(nome => nome.pos >= 15));
- console.log(nomes.some(nome => nome.pos >= 15));
