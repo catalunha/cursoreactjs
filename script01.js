b933898c6869f729a7eb37925a3120007ed3415a
catalunha <catalunha.mj@gmail.com>
2020-04-28 21:59:54 -0300

    aula22

--- script01.js
+++ script01.js
@@ -1,7 +1,16 @@
-function cadastrar(usuarios,...novos) {
-    let total = [...usuarios,...novos]
-    console.log(total);
-}
-let usuarios=['aa','bb'];
+const lista = [1, 2, 3];
 
-let novos = cadastrar(usuarios,'cc','dd');
+let novaLista = lista.map(function (item, index) {
+    return item + index;
+});
+console.log(novaLista);
+
+let soma = lista.reduce(function (total,next) {
+   return total + next; 
+});
+console.log(soma);
+
+let achei = lista.find(function (item) {
+   return item === 2; 
+});
+console.log(achei);
