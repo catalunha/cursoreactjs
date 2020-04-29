1ea7d555b2eb293b7b7b4794c7f3349b10194c48
catalunha <catalunha.mj@gmail.com>
2020-04-29 14:43:04 -0300

    aula40

--- proj01/src/App.js
+++ proj01/src/App.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
-import Membro from './components/Membro/Membro';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            status: true
+        };
+        this.sair = this.sair.bind(this);
+        this.entrar = this.entrar.bind(this);
+    }
+    sair() {
+        this.setState(
+            { status: false }
+        );
+    }
+    entrar() {
+        this.setState(
+            { status: true }
+        );
+    }
     render() {
         return (
             <div>
-               <Membro nome='Visitante'/>
+                {this.state.status
+                    ?
+                    <div>
+                        <h2>Bem vindo</h2>
+                        <button onClick={this.sair}>Sair</button>
+                    </div>
+                    :
+                    <div>
+                        <h2>Faça login</h2>
+                        <button onClick={this.entrar}>Entrar</button>
+                    </div>
+                }
             </div>
         );
     }
