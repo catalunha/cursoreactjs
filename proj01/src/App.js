62f1fab2f050e7568ac2970d4dda5f6d09d82fba
catalunha <catalunha.mj@gmail.com>
2020-04-29 11:21:35 -0300

    aula34

--- proj01/src/App.js
+++ proj01/src/App.js
@@ -1,79 +1,59 @@
-import React from 'react';
-//criando componente
-const Equipe = (props) => {
-    return (
-        <div>
-            <Sobre
-                nome={props.nome}
-                cargo={props.cargo}
-                idade={props.idade}
-            />
-            <Social fb={props.facebook} />
-            <hr />
-        </div>
+import React, { Component } from 'react';
 
-    );
+class Equipe extends Component {
+    render() {
+        return (
+            <div>
+                <Sobre
+                    nome={this.props.nome}
+                    fb1={this.props.fb}
+                />
+                <hr/>
+            </div>
+        );
+    }
 }
-const Sobre = (props) => {
-    return (
-        <div>
-            <h2>
-                Olá sou o {props.nome}
-            </h2>
-            <h2>
-                Cargo: {props.cargo}
-            </h2>
-            <h2>
-                Idade: {props.idade}
-            </h2>
-        </div>
 
-    );
+class Sobre extends Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.nome} </h1>
+                <Social fb2={this.props.fb1} />
+            </div>
+        );
+    }
 }
-const Social = (props) => {
-    return (
-        <div>
-            <a href={props.fb}>Facebook </a>
-        </div>
+// class Social extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <a href={this.props.fb2}> Facebook</a>
+//             </div>
+//         );
+//     }
+// }
 
-    );
-}
+const Social = (props) =>{
+        return (
+            <div>
+                <a href={props.fb2}> Facebook</a>
+            </div>
+        );
+    }
+
+
 function App() {
     return (
         <div>
             <h1>
                 Conheça nossa equipe:
             </h1>
-            <Equipe nome='aa' cargo='bb' idade='10' facebook='https://www.google.com.br/' />
-            <Equipe nome='aa2' cargo='bb2' idade='11' />
+            <Equipe nome='aaa' fb='https://www.google.com.br/' />
+            <Equipe nome='bb' fb='https://www.google.com.br/' />
         </div>
     );
 }
 
 export default App;
 
-
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
