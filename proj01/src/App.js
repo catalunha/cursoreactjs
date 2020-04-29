18a6b148ddcb9bd95c7341904b309dcdb9880799
catalunha <catalunha.mj@gmail.com>
2020-04-29 16:59:55 -0300

    aula45

--- proj01/src/App.js
+++ proj01/src/App.js
@@ -1,46 +1,38 @@
 import React, { Component } from 'react';
-
+import './estilo.css';
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            form: {
-                nome: '',
-                email: '',
-                senha: '',
-                sexo: '',
-            }
+            textoFrase: '',
         };
-        this.dadosForm = this.dadosForm.bind(this);
+        this.frases = ['aa', 'bb', 'cc'];
+        this.abrir = this.abrir.bind(this);
     }
-    dadosForm(event) {
-        let form = this.state.form;
-        form[event.target.name] = event.target.value;
-        this.setState(
-            { form: form }
-        );
+    abrir() {
+        let state = this.state;
+        let numQq = Math.floor(Math.random()*this.frases.length);
+        state.textoFrase = this.frases[numQq];
+        this.setState(state);
     }
     render() {
         return (
-            <div>
-                <h1>Login</h1>
-Nome: <input type='text' name='nome' value={this.state.form.nome} onChange={this.dadosForm} /><br />
-Email: <input type='text' name='email' value={this.state.form.email} onChange={this.dadosForm} /><br />
-Senha: <input type='password' name='senha' value={this.state.form.senha} onChange={this.dadosForm} /><br />
-Sexo:
-                <select name='sexo' value={this.state.form.sexo} onChange={this.dadosForm}>
-                    <option value='masc'>Masc</option>
-                    <option value='fem'>Fem</option>
-                </select>
-                <br />
-                <div>
-                    {this.state.form.nome}
-                    {this.state.form.email}
-                    {this.state.form.senha}
-                    {this.state.form.sexo}
-                </div>
+            <div className='container'>
+                <img src={require('./assets/biscoito.png')} className='img' />
+                <Botao nome='Abrir biscoito' acaoBtn={this.abrir} />
+                <h3 className='frase'>Frase: "{this.state.textoFrase}"</h3>
             </div>
 
+        );
+    }
+}
+
+class Botao extends Component {
+    render() {
+        return (
+            <div>
+                <button onClick={this.props.acaoBtn}>{this.props.nome}</button>
+            </div>
         );
     }
 }
