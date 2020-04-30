2135629b001f8eaa8ff4a78b614457c796d2c7e2
catalunha <catalunha.mj@gmail.com>
2020-04-29 21:19:59 -0300

    aula48

--- proj01/src/App.js
+++ proj01/src/App.js
@@ -1,38 +1,12 @@
 import React, { Component } from 'react';
-import './style.css';
-//https://sujeitoprogramador.com/rn-api/?api=posts
+import TodoList from './components/TodoList';
+
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            nutri: [],
-        };
-    }
-    componentDidMount() {
-        let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
-        fetch(url).then((response) => response.json()).then((json) => {
-            let state = this.state;
-            state.nutri = json;
-            this.setState(state);
-            console.log(this.state.nutri);
-        });
-    }
     render() {
         return (
-            <div className='container'>
-                <header>
-                    <strong>React Nutri</strong>
-                </header>
-                {this.state.nutri.map((item) => {
-                    return (
-                        <article key={item.id} className='post'>
-                            <strong className='titulo'>{item.titulo}</strong>
-                            <img src={item.capa} className='img'/>
-                            <p className='subtitulo'>{item.subtitulo}</p>
-                            <a href='#' className='botao'>Acessar</a>
-                        </article>
-                    );
-                })}
+            <div>
+                <h1>Lista de Tarefas</h1>
+                <TodoList/>
             </div>
         );
     }
