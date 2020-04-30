2f180f46948621fc9e7a79f1886415fe7097c170
catalunha <catalunha.mj@gmail.com>
2020-04-30 16:06:51 -0300

    aula55

--- proj01/src/App.js
+++ proj01/src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
-import Routes from './routes';
-
+import {Container,Head, Titulo,BemVindo} from './styles'
 class App extends Component {
   render() {
     return (
-      <Routes />
+      <Container>
+        <Head>
+          <Titulo>a</Titulo>
+        </Head>
+       Ola1
+       <BemVindo cor='00FF00' tamanho='15'>Bem vindo</BemVindo>
+       <BemVindo cor='00FF00' tamanho='25'>Bem vindo</BemVindo>
+      </Container>
     );
   }
 }
 
 export default App
+/*
+      <div className='container'>
+        <header className='header'>
+          <a className='titulo'>Projeto Styled</a>
+        </header>
+        <h1>Bem vindo.</h1>
+      </div>
+*/
