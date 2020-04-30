502fb28e63f162eeea5ebc5847e26ccbde9b30fb
catalunha <catalunha.mj@gmail.com>
2020-04-30 07:35:59 -0300

    aula50

--- proj01/src/routes.js
+++ proj01/src/routes.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+//components
+import Header from './components/Header'
+//pages
 import Home from './pages/Home'
 import Sobre from './pages/Sobre'
-import Header from  './components/Header'
+import Erro from './pages/Erro'
+import Produto from './pages/Produto'
 const Routes = () => {
     return (
         <BrowserRouter>
-        <Header/>
+            <Header />
             <Switch>
                 <Route exact path='/' component={Home} />
                 <Route path='/sobre' component={Sobre} />
+                <Route path='/produto/:id' component={Produto} />
+                <Route path='*' component={Erro} />
             </Switch>
         </BrowserRouter>
     );
